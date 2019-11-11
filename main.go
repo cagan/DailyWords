2b656c9be03046a159e3d5841390f8d3544cbd99
cagansit <cagan.sit@beyn.com.tr>
2019-11-11 18:57:19 +0300

    Additional information added.

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"net/http"
+)
 
-func main() {
+func init() {
+	viper.SetConfigFile(`config_local.yaml`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
 }
 
+func main() {
+	mux := http.NewServeMux()
 
+	defaultConfig := Server{
+		Addres: viper.GetString("server.address"),
+		Port:   viper.GetString("server.port"),
+	}
+
+	sv := NewServer(defaultConfig)
+
+	mux.HandleFunc("/hello", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println("Hello World")
+	})
+
+	err := StartServer(*sv, mux)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Server started on port: %s", viper.Get("server.port"))
+}
--- server.go
+++ server.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+)
+
 type Server struct {
+	Addres string 
+	Port string
 }
 
-func StartServer() {
+var defaultConfig Server
+
+func init() {
+	if viper.GetBool(`debug`) {
+
+	}
+}
+
+func NewServer(server Server) *Server {
+	server := new(Server)
+	return server
+}
+
+func StartServer(sv Server, mux *http.ServeMux) error {
+	err := http.ListenAndServe(fmt.Sprintf("%s:%s", sv.Addres, sv.Port), mux)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
