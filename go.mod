d1d54149feb33dd373f3345b8d866f2cf12e55fa
cagans42 <cagan.sit@gmail.com>
2019-11-14 22:12:26 +0300

    .env file added

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/jinzhu/gorm v1.9.11 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/mingrammer/go-todo-rest-api-example v0.0.0-20190527014715-ae46b4d42804 // indirect
 	github.com/spf13/viper v1.5.0
 	github.com/urfave/cli v1.22.1 // indirect
--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"log"
 	"net/http"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 func init() {
 	viper.SetConfigName("config")
 	viper.SetConfigFile(`config.json`)
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 }
 
 func main() {
 	mux := http.NewServeMux()
+	env := os.Getenv("ENVIRONMENT")
+	if env == "dev" {
+		fmt.Println("Development Environment")
+	}
 
 	defaultConfig := Server{
 		Addres: viper.GetString("server.address"),
